1d853b6276ffb5aaea116294152dba97bfd8f2de
Thomas Murphy <Tom@Thomass-MacBook-Air.local>
2017-09-16 16:32:44 -0400

    initial files

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+var friendsArray = [{
+  'name': 'SewerWeasel',
+  'photo': 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjt8JDF5KLWAhWG2SYKHe8VDz8QjRwIBw&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FWeasel&psig=AFQjCNFMn0lUO8qh5cYfZTxUGEG4PgHFcg&ust=1505413510014597',
+  'scores': [
+    5,
+    1,
+    4,
+    4,
+    5,
+    1,
+    2,
+    5,
+    4,
+    1
+  ]
+}];
+
+module.exports = friendsArray;
